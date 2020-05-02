0f68a4c5220f98ccd51f93400de0b7487b2c4a5a
hurrialice <zhangq07@yahoo.com>
2020-05-02 01:56:19 -0400

    update to zipcode level data

--- read_data.R
+++ read_data.R
@@ -31,20 +31,26 @@ z_df <- do.call(rbind, zlist)
 
 colnames(z_df)[1] <- "ZIP"
 # write_csv(z_df, "cleaned_data/zip_count_rate_temp.csv")
-
+library(dplyr)
 
 
 # read zip 
 
 zip_dict <- read_csv("virus_data/ZIP-COUNTY-FIPS_2017-06.csv")
 z_demo_county <- read_csv("virus_data/county_data.csv") %>% mutate(STCOUNTYFP = paste0(statefip, fipcode))
-# z_demo <- read_csv("Demographic_Statistics_By_Zip_Code.csv")
+z_demo <- read_csv("~/Downloads/Demographic_Statistics_By_Zip_Code.csv") %>% mutate(ZIP = `JURISDICTION NAME`)
+z_pop <- read_csv("~/Documents/population_zip.csv", col_names = FALSE) %>% mutate(ZIP = as.numeric(X2))
 # colnames(z_demo)[1] <- "ZIP"
 # z_state <- read_csv("state_data.csv")
 
 
+
+
+
+
 library(dplyr)
 dft <-left_join(z_df, zip_dict)
-dft <- left_join(dft, z_demo_county)
+dft <- left_join(dft, z_demo)
+dft <- left_join(dft, z_pop)
 write_csv(dft, "cleaned_data/zip_count_rate_temp.csv")
 
